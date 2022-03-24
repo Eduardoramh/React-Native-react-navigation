c5daed5ff53c5f75dcf19937eef00848a0711d84
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-03-24 15:22:30 -0600

    Instalacion y configuracion react navigation 5

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text, SafeAreaView} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <SafeAreaView>
+        <Text>Hello motherfucker</Text>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+}
