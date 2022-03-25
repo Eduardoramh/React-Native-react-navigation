7f286f06fa5dfb5d2f35aa5978db6116eb2673d3
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-03-25 13:33:40 -0600

    Combinacion TabNavigation y StackNavigation

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,29 +1,15 @@
 import React from "react";
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from "../screens/Home"
-import About from "../screens/About"
-import Contact from "../screens/Contact"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeStack from "./HomeStack"
+import ContactStack from "./ContactStack";
 
-// const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name = "home" component={Home} options={{title: 'Home'}} />
-            <Tab.Screen name = "about" component={About} options={{title: 'About'}} />
-            <Tab.Screen name = "contact" component={Contact} options={{title: 'Contact'}} />
+            <Tab.Screen name = "home" component={HomeStack} options={{title: 'Home'}} />
+            <Tab.Screen name = "contact" component={ContactStack} options={{title: 'Contact'}} />
         </Tab.Navigator>
     );
 }
-
-// export default function Navigation() {
-//     return(
-//         <Stack.Navigator>
-//             <Stack.Screen name="home" component={Home} options={{title: 'Home'}} />
-//             <Stack.Screen name="about" component={About} options={{title: 'About'}} />
-//             <Stack.Screen name="contact" component={Contact} options={{title: 'Contact'}} />
-//         </Stack.Navigator>
-//     );
-// }
