efcba965e25d018728a8acf497123a4d151d71fd
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-03-25 12:29:12 -0600

    Navegacion entre screens

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,10 +1,13 @@
 import React from "react"
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-export default function About() {
+export default function About(props) {
+    const {navigation} = props;
+
     return(
         <View>
             <Text style={styles.about}>ESTAMOS EN ABOUT</Text>
+            <Button title="Ir a Contact" onPress={() => navigation.navigate('contact')} />
         </View>
     )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,14 @@
 import React from "react"
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-export default function Home() {
+export default function Home(props) {
+    const {navigation} = props;
+
     return(
         <View>
             <Text style={styles.home}>ESTAMOS EN LA HOME</Text>
+            <Button title="Ir About" onPress={() => navigation.navigate('about')} />
+            <Button title="Ir a Contact" onPress={() => navigation.navigate('contact')} />
         </View>
     )
 }
